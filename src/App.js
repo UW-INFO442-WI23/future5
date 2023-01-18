890f23187dc675199e6752ff1e56267ced118a35
Jared Lim <jorlim7@uw.edu>
2023-01-18 11:08:47 -0800

    Added homepage

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom/client';
 
+const HomePage = () => {
+    let array = [0, 5, 7, 9, 10]
+    let total = 0
+    array.forEach(element => {
+        total += element
+    })
+    let element = <h1>The total is {total}</h1>
+    return element
+    
+}
+
+export default HomePage
